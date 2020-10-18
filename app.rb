876eb29de0ed6337b95f330ecf8a1818e1cac5c6
Manoj Kumar <manojvirat457@gmail.com>
2020-10-18 17:27:42 +0530

    trainee works

--- app.rb
+++ app.rb
@@ -3,17 +3,25 @@ require './dal'
 
 class App < Roda
     plugin :json
+    plugin :render
 
     route do |r|
 
         r.root do
-            "Welcome to buzzn"
+            "<h1>Welcome to buzzn</h1>"
         end
 
+        # to render html page
+        r.is "login" do
+            render('form')
+        end
+
+        # to make it as rest
         r.is "user" do
             instance = Dal.new
             puts r.params['name']
             instance.updateUser(r.params['name'], r.params['mail'])
+            # r.params['name'] + "->" + r.params['mail']
         end
 
     end
